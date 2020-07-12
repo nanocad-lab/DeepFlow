033ea85fccced49d0317dfc0003238142c143043
Saptadeep Pal <saptadeep@ucla.edu>
2020-07-11 22:16:19 -0700

    Update dist_gemm_tf2.py

--- dist_gemm_tf2.py
+++ dist_gemm_tf2.py
@@ -17,6 +17,32 @@ def matmul(a,b,device):
     with tf.device(device):
         return tf.matmul(a, b)
 
+@tf.function
+def RC(m, k, n, kern_para_b, num_devices, a_shards, b_shards, c_final):
+        #c_final = make_variables(m,n,tf.random_normal_initializer(mean=1., stddev=0.2),'float32')
+        c = tf.split(c_final, kern_para_b, axis=1)
+        for i in range(kern_para_b):
+            curr_device = '/device:gpu:' + str(i % num_devices)
+            print(curr_device)
+            c[i] = matmul(a_shards, b_shards[i], curr_device)
+            #c[1] = matmul(a_shards[0], b_shards[1], '/device:gpu:1')
+        with tf.device('/device:gpu:0'):
+            c_final = tf.concat([ci for ci in c], axis=1)
+        return c_final
+
+@tf.function
+def CR(m, k, n, kern_para_a, num_devices, a_shards, b_shards):
+    c_int = {}
+    #start = tf.timestamp()
+    for i in range(kern_para_a):
+        curr_device = '/device:gpu:' + str(i % num_devices)
+        c_int[i] = matmul(a_shards[i], b_shards[i], curr_device)
+        #print(c_int[i])
+
+    with tf.device('/device:gpu:0'):
+        c_final = tf.math.add_n([c_int[c_i] for c_i in c_int])
+    return c_final
+
 def main():
     # Graph:
     # a, b, weights, bias inputs
@@ -26,8 +52,8 @@ def main():
     n = None
     ###kern_para_a >1 for CR: kern_para_b>1 for RC. This isn't entirely true though
     ###TODO: Implement complete tiling for RC and CR
-    kern_para_a = 1
-    kern_para_b = 4
+    kern_para_a = 4
+    kern_para_b = 1
     b_dims = (k, n)
     c_dims = (m, n)
 
@@ -43,6 +69,9 @@ def main():
     a = make_variables(m,k,tf.random_normal_initializer(mean=1., stddev=0.),'float16')
     b = make_variables(k,n,tf.random_normal_initializer(mean=1., stddev=0.),'float16')
 
+    writer = tf.summary.create_file_writer("tensorboard.log")
+    tf.summary.trace_on(graph=True, profiler=True)
+
     if kern_para_a > 1:
         a_shards = tf.split(a, kern_para_a, axis=1)
         b_shards = tf.split(b, kern_para_a, axis=0)
@@ -53,28 +82,13 @@ def main():
 
     c_final = make_variables(m,n,tf.random_normal_initializer(mean=1., stddev=0.2),'float32')
     if ((kern_para_a == 1) and (kern_para_b >1)):
-        c = tf.split(c_final, kern_para_b, axis=1)
-        start = tf.timestamp()
-        for i in range(kern_para_b):
-            curr_device = '/device:gpu:' + str(i % num_devices)
-            print(curr_device)
-            c[i] = matmul(a, b_shards[i], curr_device)
-            #c[1] = matmul(a_shards[0], b_shards[1], '/device:gpu:1')
-        with tf.device('/device:gpu:0'):
-            c_final = tf.concat([ci for ci in c], axis=1)
-        tot = tf.timestamp() - start
+        RC(m, k, n, kern_para_b, num_devices, a, b_shards, c_final)
     elif ((kern_para_b == 1) and (kern_para_a >1)):
-        c_int = {}
-        start = tf.timestamp()
-        for i in range(kern_para_a):
-            curr_device = '/device:gpu:' + str(i % num_devices)
-            c_int[i] = matmul(a_shards[i], b_shards[i], curr_device)
-            #print(c_int[i])
-
-        with tf.device('/device:gpu:0'):
-            c_final = tf.math.add_n([c_int[c_i] for c_i in c_int])
-        tot = tf.timestamp() - start
-    print(tf.shape(c_final), tot)
+        CR(m, k, n, kern_para_a, num_devices, a_shards, b_shards)
+    with writer.as_default():
+        tf.summary.trace_export(name="trace",step=0,profiler_outdir="tensorboard.log")
+    print(tf.shape(c_final))
+    writer.flush()
     #print(a, b, c_final)
 
 
