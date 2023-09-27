e107679c66fcfe5965abae1ac445f021794bb69f
Joyjit Kundu <kundu16@imec.be>
2023-09-27 10:29:37 +0200

    clean-up, TP degree is passed into DeepFlow

--- scripts/amped_scripts/buildRun.sh
+++ scripts/amped_scripts/buildRun.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+#-----------------------------------------------------------------------------------
 # Running AMPeD to spit out the configs
-# Go to AMPeD
-
+# Go to AMPeD repo
+#-----------------------------------------------------------------------------------
 dir="/imec/other/csainfra/kundu16/public/AMPeD"
 rm $dir/output_files/*.txt #removing the old txt files
 
@@ -12,7 +12,9 @@ conda activate amped #look at https://github.com/CSA-infra/AMPeD to create your 
 python -m amped.main
 # This generates to files: 2023-09-20_16-35-36_config_summary.txt, 2023-09-20_16-35-36_training_time_breakdown.txt
 
-#cd /imec/other/csainfra/kundu16/DeepFlow/scripts/amped_scripts
+#-----------------------------------------------------------------------------------
+# parsing amped outputs to create the taskgraph and GEMM dimensions
+#-----------------------------------------------------------------------------------
 cd $dir/output_files
 #saving the output filenames
 array=(a,b)
@@ -31,10 +33,14 @@ cd /imec/other/csainfra/kundu16/DeepFlow/scripts/amped_scripts
 python mat_dims_ampedToDF.py --config_filename ${array[0]}
 conda deactivate
 
+#--------------------------------------------------------------------------------------
 # run DeepFlow; change parallelization in run.sh if needed
+#--------------------------------------------------------------------------------------
 source /imec/other/csainfra/kundu16/DF/bin/activate #look at https://github.com/nanocad-lab/DeepFlow for the virtual env "DF"
-source run.sh
+source run.sh $dir/output_files/${array[0]} #passing the config file obtained from AMPeD to extract the TP_intra degree
 
-# calculate total time
+#--------------------------------------------------------------------------------------
+# stitches the elapsed times to calculate total time
+#--------------------------------------------------------------------------------------
 python cal_time.py --config_filename ${array[0]} --breakdown_filename ${array[1]} 
 deactivate
--- scripts/amped_scripts/run.sh
+++ scripts/amped_scripts/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+TP_intra=$(grep "intra_node_tensor_parallel_degree" $1 | awk -F ':' '{gsub(/[^0-9]/, "", $2); print $2}' | tr -d '[:space:]')
+echo "TP intra degree: $TP_intra"
+
 # Input data file
 input_file="/imec/other/csainfra/kundu16/DeepFlow/scripts/amped_scripts/mat_dims_amped.txt"
 
@@ -19,7 +22,7 @@ while IFS= read -r line; do
     value3=$(echo "$line" | awk '{print $3}')
 
     # Submit a job using python perf.py with extracted values as arguments
-    python "$RUNDIR"/perf.py --exp_config "$CONFIG_DIR"/v100.yaml --exp_dir "$OUTDIR"/LLM/ --debug True --gemm True --t RC --kp1 16 --kp2 1 --m "$value1" --n "$value2" --k "$value3"
+    python "$RUNDIR"/perf.py --exp_config "$CONFIG_DIR"/v100.yaml --exp_dir "$OUTDIR"/LLM/ --debug True --gemm True --t RC --kp1 $TP_intra --kp2 1 --m "$value1" --n "$value2" --k "$value3"
 
     # You can add sleep between job submissions if needed
     # sleep 1
