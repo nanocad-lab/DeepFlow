761e2e93b640bb6d421c5dec780eb9d41075fcc6
gkarfakis19 <george.karfakis@gmail.com>
2025-03-06 14:16:28 -0800

    Added UV management support and refactored some LLM scriptware to use relative paths.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Input data file
-input_file="/home/nanoproj/ravit/DeepFlow/scripts/mat_dims.txt"
+input_file="mat_dims.txt"
 
 # Relevant directories
-RUNDIR="/home/nanoproj/ravit/DeepFlow"
-CONFIG_DIR="/home/nanoproj/ravit/DeepFlow/configs/new-configs"
-OUTDIR="/home/nanoproj/ravit/DeepFlow/results/output"
+RUNDIR="../"
+CONFIG_DIR="../configs/new-configs"
+OUTDIR="../output"
 
 # Remove the old files
 rm -rf "$OUTDIR"/LLM/*
@@ -19,7 +19,7 @@ while IFS= read -r line; do
     value3=$(echo "$line" | awk '{print $3}')
 
     # Submit a job using python perf.py with extracted values as arguments
-    python "$RUNDIR"/perf.py --exp_config "$CONFIG_DIR"/v100_no_frac.yaml --exp_dir "$OUTDIR"/LLM_no_frac/ --debug True --gemm True --t RC --kp1 16 --kp2 1 --m "$value1" --n "$value2" --k "$value3"
+    python3 "$RUNDIR"/perf.py --exp_config "$CONFIG_DIR"/waferscale_20v100_80hbm.yaml --exp_dir "$OUTDIR"/LLM/ --debug True --gemm True --t RC --kp1 16 --kp2 1 --m "$value1" --n "$value2" --k "$value3"
 
     # You can add sleep between job submissions if needed
     # sleep 1
